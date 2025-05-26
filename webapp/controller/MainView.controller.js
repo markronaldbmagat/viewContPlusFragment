2ea5acc015f5e75b07ddbc48de5d30042c6707c3
Mark Ronald Magat <mark.ronald.b.magat@accenture.com>
2025-05-26 02:49:21 +0000

    Initial Commit

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,47 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+
+    return Controller.extend("com.training.exer1.controller.MainView", {
+        onInit() {
+        },
+        // pinaltan ng onPressCheckout sa Step 12 ito..
+        onAddItem: function () {
+            this.fnDisplayMsg("Add button pressed");
+        },
+        
+        fnDisplayMsg: function (sMsg) {
+            MessageToast.show(sMsg);
+        },
+        onChangeMOP: function (oEvent) {
+            var sSelectedKey = oEvent.getSource().getSelectedKey(); // pwedeng paghiwalayin ito.. bale ung gnagawa ng getSource is to 'get' kung aling combobox; getSelectedKey naman is to 'get' kung anong key
+            //parang ganto:
+            //  var oComboBox = oEvent.getSource(); // This gets the ComboBox that was changed
+            //  var sSelectedKey = oComboBox.getSelectedKey();
+
+            // var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");   ung getParameter daw luma na.
+            var oView = this.getView();
+        
+            var oMobileLabel = oView.byId("idLblPhone");
+            var oMobileInput = oView.byId("idInputPhone");
+        
+            var bShow = (sSelectedKey === "GCASH");
+            oMobileLabel.setVisible(bShow);         //b = boolean; Show not a reserved keyword but a standard practise for true or false. no need to declare ito
+            oMobileInput.setVisible(bShow);
+        },
+        onPressCheckout: function () {
+            //this.fnDisplayMsg("Add button pressed");
+            var oInputFNameValue = this.getView().byId("idInptFName").getValue();
+
+            // Check if first name is blank
+            if (oInputFNameValue === ""){
+                sap.m.MessageToast.show("Required Field is blank"); 
+            }
+        }       
+    });
+});
+// in summary, this exercise will not check which row was clicked.
+// chinicheck nya however kung may laman ba ung input field na first name demonstrating kung pano mag implement ng validation.
+// last step is to use i18n folder para tanggalin ung mga hardcodes.. parking this for now to prioritize ung ibang activities..
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/training/exer1/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
